0cc70d79a6d428b4e8dc6a8bf0372ee2644bbea3
Jeff Wong <jeffmjwong@gmail.com>
2018-03-18 21:24:09 +1000

    Add background video to home page

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const homePage = document.querySelector('.home-container.home-page')
+const homePage = document.querySelector('.home-container.home-page');
+const homePageBackgroundVideo = document.querySelector('.home-page-background-video');
 const nextButton = document.querySelector('.button-at-homepage');
 const homeButton = document.getElementsByClassName('footer-home-button');
 const headerBar = document.querySelector('.header-bar');
 const menuButton = document.querySelector('.header-menu-button');
 const dropdownContent = document.querySelector('.header-menu-dropdown-content');
 const allPageMinusHome = document.getElementsByClassName('main-container');
-const firstPage = document.querySelector('.main-container.first-page')
+const firstPage = document.querySelector('.main-container.first-page');
 const firstPageRightBar = document.querySelector('.first-page .sliding-bar');
-const secondPage = document.querySelector('.main-container.second-page')
+const secondPage = document.querySelector('.main-container.second-page');
 const secondPageLeftBar = document.querySelectorAll('.second-page .sliding-bar')[0];
 const secondPageRightBar = document.querySelectorAll('.second-page .sliding-bar')[1];
-const thirdPage = document.querySelector('.main-container.third-page')
+const thirdPage = document.querySelector('.main-container.third-page');
 const thirdPageLeftBar = document.querySelectorAll('.third-page .sliding-bar')[0];
-const thirdPageRightBar = document.querySelectorAll('.third-page .sliding-bar')[1];
-const forthPage = document.querySelector('.main-container.forth-page')
-const forthPageLeftBar = document.querySelector('.forth-page .sliding-bar');
+
+homePageBackgroundVideo.addEventListener('canplay', () => {
+  setTimeout(() => {
+    homePageBackgroundVideo.play();
+  }, 3200);
+});
 
 menuButton.addEventListener('click', () => {
   dropdownContent.classList.toggle("show");
@@ -60,14 +64,4 @@ secondPageRightBar.addEventListener('click', () => {
 thirdPageLeftBar.addEventListener('click', () => {
   thirdPage.style.display = "none";
   secondPage.style.display = "grid";
-});
-
-thirdPageRightBar.addEventListener('click', () => {
-  thirdPage.style.display = "none";
-  forthPage.style.display = "grid";
-});
-
-forthPageLeftBar.addEventListener('click', () => {
-  forthPage.style.display = "none";
-  thirdPage.style.display = "grid";
 });
